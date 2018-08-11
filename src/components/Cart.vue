7ed9b1d037bda37749c2fe449e9ff97e5988099f
pushenko-alex <fun.work.js@gmail.com>
2018-08-11 15:25:50 +0300

    Single Page Application Эмуляция интернет-магазина на Vue.js

---
+++ src/components/Cart.vue
@@ -0,0 +1,135 @@
+<template>
+	<div>
+		<div class="title">
+			<h1>Корзина</h1>
+			<div>
+				<div>В корзине <span style="color:green; font-size: 20px">{{cnt}}</span>шт. товаров</div>
+				
+			</div>
+		</div>
+		
+		<hr>
+		<div v-if="empty"
+			 class="alert alert-warning">
+			Your cart is empty
+		</div>
+		<template v-else>
+			<table class="table table-bordered table-hover">
+				<thead>
+					<tr>
+						<th>Наимнование</th>
+						<th>Цена 1шт.</th>
+						<th>Кол-во</th>
+						<th>Сумма</th>
+						<th>Действие</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr v-for="product in productsInCart">
+						<td>{{ product.title }}</td>
+						<td>{{ product.price }} руб.</td>
+						<td style="display: flex;">
+							<span>{{ product.count }}</span>
+							<button class="btn btn-warning" 
+								@click="minus(product)"
+						>-1</button>
+						<button class="btn btn-success" 
+								@click="plus(product)">+1</button>
+								<span>x</span>
+								 <input type="text" 
+										:value="' ' + product.count"
+										@input="onInput($event, product)"
+										>
+						</td>
+						<td style="font-size:18px">{{ product.sum }} руб.</td>
+						<td><button class="btn btn-danger"
+									@click="remove(product)"
+						>
+					Х
+				</button></td>
+					</tr>
+				</tbody>	
+			</table>
+			<div class="bottom">
+				
+				<button	class="btn btn-danger"
+					@click="removeAll"
+					>Удалить всё
+				
+				</button>
+				<div>Всего <span style="color:red; font-size: 25px">{{totalPrice}}</span> руб.</div>
+				
+			</div>
+			
+			
+		</template>
+	</div>
+</template>
+
+<script>
+	import {mapGetters} from 'vuex';
+	import {mapActions} from 'vuex';
+
+	export default {
+		computed: {
+			...mapGetters('cart', {
+				productsInCart: 'products',
+				totalPrice: 'totalPrice',
+				cnt:'totalCount'
+            }),
+
+			empty() {
+				return this.productsInCart.length === 0;
+            },
+	    },
+
+		methods: {
+			...mapActions('cart', {
+				remove: 'removeFromCart',
+				removeAll:'removeAll',
+				plus:'add',
+				minus:'minus'
+            }),
+
+			onInput(e, product) {
+				this.$store.commit('cart/onInput', {
+					value: e.target.value,
+					product: {
+                        ...product
+                    }
+				});
+			}
+        },
+}
+</script>
+<style scoped>
+	.title{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.title div {
+		font-size: 20px;
+	}
+	button {
+		margin: 5px;
+
+	}
+	span {
+		font-size: 15px;
+		margin-right: 10px;
+		margin-top:10px;
+	}
+	input{
+		width: 40px;
+		height: 42px;
+		border-radius: 5px;
+	}
+	.bottom{
+		display: flex;
+	}
+	.bottom button {
+		margin-right: 10px; 
+	}
+</style>
---
+++ src/store/modules/cart.js
@@ -0,0 +1,109 @@
+export default {
+    namespaced: true,
+
+	state: {
+		products: []
+	},
+	getters: {
+		products(state){
+			return state.products;
+		},
+		totalCount(state) {
+			/*Расчет общего кол-ва товаров в корзине */
+
+            let total = 0;
+            let i = state.products.length;
+            while (i--) {
+            	total += state.products[i].count;
+				state.products[i].sum = state.products[i].count*state.products[i].price;
+            }
+			return total;
+		},
+		totalPrice(state){
+			/*Расчет общей суммы товаров в корзине */
+
+			let total = 0;
+			for(let i = 0, len = state.products.length;  i<len; i++){
+				total += state.products[i].sum;
+			}
+			return total;
+		}
+	},
+
+	mutations: {
+		add(state, product) {
+			/*Добавить товар в корзину если его нет, если есть накрутить счетчик +1*/
+            const index = state.products.findIndex((subProduct) => {
+                return subProduct.id_product === product.id_product;
+            });
+
+            if (index === -1) {
+                state.products = [{
+                    ...product
+                }, ...state.products];
+            } else {
+                state.products[index].count += 1;
+            }
+        },
+
+		removeFromCart(state, product) {
+			/*Удалить товар из корзины*/
+
+            const index = state.products.findIndex((subProduct) => {
+                return subProduct.id_product === product.id_product;
+            });
+            
+            state.products = state.products.filter((product) => {
+                return product !== state.products[index];
+            });
+        },
+
+		removeAll(state) {
+			/*Удалить все товары из корзины*/
+
+			state.products = [];
+        },
+
+		minus(state, product) {
+			/*Уменьшить кол-во товара на 1*/
+
+            const index = state.products.findIndex((subProduct) => {
+                return subProduct.id_product === product.id_product;
+            });
+
+            if (index === -1 || state.products[index].count <= 1) {
+                return;
+            }
+
+            state.products[index].count -= 1;
+        },
+
+		onInput(state, payload) {
+			/* Форма ввода произвольного кол-ва товара*/
+			
+			const index = state.products.findIndex((product) => {
+                return product.id_product === payload.product.id_product;
+			});
+			state.products[index].count = Number(payload.value);
+			if(isNaN(state.products[index].count) || state.products[index].count === 0){
+				state.products[index].count = 1;
+			}
+			
+							
+		}
+	},
+	actions: {
+		add(store, product) {
+			store.commit('add', product);
+		},
+		removeFromCart(store, product) {
+			store.commit('removeFromCart', product);
+		},
+		removeAll(store) {
+			store.commit('removeAll');
+		},
+		minus(store, product) {
+			store.commit('minus', product);
+		}
+	}
+};
